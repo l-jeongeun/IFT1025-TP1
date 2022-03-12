b146ca0c4b7a68ee34dd62ba254ed12f974b4645
mariannep01 <91166970+mariannep01@users.noreply.github.com>
2022-03-12 00:52:58 -0500

    Update Antilope.java

--- Antilope.java
+++ Antilope.java
@@ -1,1 +1,50 @@
+// Fichier :     Antilope.java
+// Création:     
+// Auteurs :     
+//
+// Ce code n'est pas protégé par un copyright.
+// 
+// Historique :
+//  Créé pour le cours IFT1025 H22
+//
 
+/**
+   class Antilope
+    extends Animal
+
+**/
+
+public class Antilope extends Animal {
+    double facteurCroissanceAntilopes;
+    Herbe herbe;
+    EcoSysteme ecosystem;
+    Savane savane;
+    Antilope(double facteurCroissanceAntilopes ){
+        super();
+        this.facteurCroissanceAntilopes = facteurCroissanceAntilopes;
+        setProie(true);
+
+    }
+    
+    @Override
+    public int getAgeMax() {
+        return 15;
+    }
+    @Override
+    public int getAgeMature() {
+        return 2;
+    }
+    
+    @Override
+    public Animal accoucher() {
+        return new Antilope(facteurCroissanceAntilopes);
+    }
+    @Override
+    public void manger() {
+        double nouvelleMasseHerbe = herbe.getMasse() - (2 * ecosystem.masseProies()) ;
+        herbe.setMasse(nouvelleMasseHerbe);
+
+    }
+
+
+}
