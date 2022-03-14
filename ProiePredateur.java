27cda5ad32202d2015205c6fcbec0d09fcc8e3fc
mariannep01 <91166970+mariannep01@users.noreply.github.com>
2022-03-14 01:29:40 -0400

    Update ProiePredateur.java

--- ProiePredateur.java
+++ ProiePredateur.java
@@ -26,6 +26,8 @@ public interface ProiePredateur {
     public boolean estMature(); // animal is mature
     public void setProie( boolean proie ); // set animal mode to prey
     public boolean estProie(); // animal is a prey
+    public void setPredateur( boolean predateur ); // set animal mode to predator
+    public boolean estPredateur(); // animal is a predator
     public double getMasse(); // get animal's mass
     public void setMasse( double masse ); // set animal's mass
     public void setAge( int age ); // set animal's age
@@ -33,4 +35,3 @@ public interface ProiePredateur {
     public int getAgeMax(); // get animal's maximum age
     public int getAgeMature(); // get animal's mature age
 }
-
