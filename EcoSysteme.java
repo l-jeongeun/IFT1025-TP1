2d8e6a65382ec4ecfaeda740adecd31492a1d1f0
mariannep01 <91166970+mariannep01@users.noreply.github.com>
2022-03-27 17:43:05 -0400

    Update EcoSysteme.java

--- EcoSysteme.java
+++ EcoSysteme.java
@@ -16,12 +16,15 @@
    @see 
 **/
 
+import java.util.ArrayList;
+
 public interface EcoSysteme {
     public int getNombreProies(); // number of current preys
     public int getNombrePredateurs(); // number of current predators
     public int getNombreProiesMatures(); // number of current mature preys
     public int getNombrePredateursMatures(); // number of current mature predators
     public int getNombreProiesChassables(); // number of current chaseable preys
+    public ArrayList<Animal> getIndividus(); // animals in the population
     public double masseProies(); // current total mass of preys
     public double massePredateurs(); // current toral mass of predators
     public void vieillir(); // getting older
@@ -29,4 +32,3 @@ public interface EcoSysteme {
     public void reproduire(); // reproducing
     public void melanger(); // mix the list of animals
 }
-
