66bc0484c3948ceee38a83c05d56589a1b8cb6f4
mariannep01 <91166970+mariannep01@users.noreply.github.com>
2022-03-27 17:45:27 -0400

    Update Population.java

--- Population.java
+++ Population.java
@@ -19,29 +19,26 @@ import java.lang.Iterable;
 import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Random;
-import java.lang.Math;
 
 // Defines a population of herb and animals (preys and predators), iterable
 public class Population implements EcoSysteme, Iterable<Animal> {
-    private ArrayList<Animal> individus = new ArrayList<>(); // concatener proies et predateurs
-    int nombreAntilopesMatures = 0;
-    int nombreLionsMatures = 0 ;
+    private ArrayList<Animal> individus = new ArrayList<>(); 
+   
+    int nombreAntilopesMatures;
+    int nombreLionsMatures  ;
     Herbe herbe;
     
-
-    
-   
-
     public Population( Herbe herbe, ArrayList<Animal> proies, ArrayList<Animal> predateurs ) {
-        // A completer
-        this.herbe = herbe;
+	
+    this.herbe = herbe;
 
-        for(int i = 0; i<(proies.size()); i++){
-            this.individus.add(proies.get(i));
-        }
-        for(int i = 0; i<(predateurs.size()); i++){
-            this.individus.add(predateurs.get(i));
-        }
+    for(int i = 0; i<(proies.size()); i++){
+        this.individus.add(proies.get(i));
+    }
+    for(int i = 0; i<(predateurs.size()); i++){
+        this.individus.add(predateurs.get(i));
+    }
+    
     }
     
     public Iterator<Animal> iterator(){
@@ -49,66 +46,69 @@ public class Population implements EcoSysteme, Iterable<Animal> {
     }
     
     // number of current preys
-    public int getNombreProies()
-    {
-        int count = 0;
-        for(Animal a : this.individus) {
-            if(a.estProie() && a.estVivant())    count++;
+    public int getNombreProies(){
+        int nombreDeProies = 0;
+        for( Animal a : this.individus ) {
+            if(a.estProie() && a.estVivant()){
+                nombreDeProies++;
+            }
         }
-        return count;
+        return nombreDeProies;
     }
 
     // number of current predators
-    public int getNombrePredateurs()    
-    {
-        int count = 0;
-        for(Animal a : this.individus) {
-            if(a.estPredateur() && a.estVivant())    count++;
+    public int getNombrePredateurs(){
+        int nombreDePredateurs = 0;
+        for( Animal a : this.individus ) {
+            if(a.estPredateur() && a.estVivant()){
+                nombreDePredateurs++;
+            }
         }
-        return count;
+        return nombreDePredateurs;
     }
 
     // number of current mature preys
     public int getNombreProiesMatures(){
+       int nombreAntilopesMatures = 0;
         for( Animal a : this.individus ) {
-            if( a.estProie() && a.estVivant() && a.estMature() ) nombreAntilopesMatures++;               
+            if( a.estMature() && a.estVivant() && a.estProie()) {
+                nombreAntilopesMatures++;    
+            }              
         }
+    
+        //System.out.println(this.nombreAntilopesMatures+"Antilope mature");
         return nombreAntilopesMatures;
     }
 
     // number of current mature predators
     public int getNombrePredateursMatures(){
-        for( Animal a : this.individus ) {
-            if( a.estPredateur() && a.estVivant() && a.estMature() ) nombreLionsMatures++;               
+        nombreLionsMatures = 0;
+        for( Animal a : this.individus) {
+            if( a.estMature()  && a.estVivant() && a.estPredateur()) {
+                nombreLionsMatures++;
+            }               
         }
+        
         return nombreLionsMatures;
     }
 
     // number of current chaseable preys
-    public int getNombreProiesChassables()
-    {
-        //seulement 20% des antilopes vivantes sont chassable
-        int  nombreProiesVivante = 0;
-        for( Animal a : this.individus ) {
-            if( a.estProie() && a.estVivant() ) nombreProiesVivante++;               
-        }
-
-        int nombreProiesChassable = (int)Math.floor(nombreProiesVivante*0.2);
-           
-        return nombreProiesChassable;
+    public int getNombreProiesChassables(){
+        //seulement 20% des antilopes vivantes sont chassables  
+        int nombreProiesChassable = (int)(this.getNombreProies()*0.2);
+           return nombreProiesChassable;
     }
-
+    
     // current total mass of preys
     public double masseProies(){
         double masseTotale = 0;
         //get the mass of every prey
-        for( Animal a : this.individus ) {
-            if(!a.estProie() || !a.estVivant()) continue;
-
+        for( Animal a : individus ) {
+            if(a.estProie() & a.estVivant()){   
             double masse = a.getMasse();
             masseTotale += masse;
+            }
         }
-
         return masseTotale;
     }
 
@@ -116,13 +116,12 @@ public class Population implements EcoSysteme, Iterable<Animal> {
     public double massePredateurs(){
         double masseTotale = 0;
         //get the mass of every predator
-        for( Animal a : this.individus ) {
-            if(!a.estPredateur() || !a.estVivant()) continue;
-
+        for( Animal a : individus ) {
+            if(a.estPredateur() & a.estVivant()){   
             double masse = a.getMasse();
             masseTotale += masse;
+            }
         }
-
         return masseTotale;
     }
 
@@ -132,134 +131,92 @@ public class Population implements EcoSysteme, Iterable<Animal> {
         herbe.vieillir();
         //les animaux vieillissent en ajoutant un an a leur age et s'ils ont atteint l'age max, ils meurt
         for( Animal a : this.individus ) {
-            a.vieillir();
+            if(!a.estVivant()) continue;
+            a.vieillir();         
         }
-        retirerAnimauxMorts();
+        retirerAimauxMorts();
     }
 
-    // chasing
-    public void chasser()
-    {
+    
+      // chasing
+      public void chasser(){
         melanger();
         int nombreDeProiesChassees = 0;
         int chassable = this.getNombreProiesChassables();
         for( Animal a : this.individus ) {
-            if (!a.estVivant()) {
-                continue;
-            }
-
+            if (!a.estVivant()) continue;
             //si c'est un predateur on fait manger le predateur
             if( a.estPredateur()){
-                //iterer a travers la list
-                double totalMasseAntilopeMangee = 0;
-                double doubleMassePredateur = a.getMasse() *2;
-                int i = 0 ;
-                
-
+                double doubleMassePredateur = a.getMasse() *2;                
                 for(Animal b : this.individus) {
-                    if((totalMasseAntilopeMangee >= doubleMassePredateur) ||
-                        (i >= this.individus.size()) || 
-                        (nombreDeProiesChassees >= chassable))   break;
-
-                    if (b.estProie() && b.estVivant()){
-                        totalMasseAntilopeMangee += b.getMasse();
+                    if((nombreDeProiesChassees >= chassable))   break;
+                    if (b.estProie() && b.estVivant() && doubleMassePredateur>0 ){
+                        doubleMassePredateur -= b.getMasse();
                         nombreDeProiesChassees++;
-                        //individus.remove(i);
                         b.mourir();
                     }
                 }
 
-                //si le lion n'a pas mangE la masse suffisante elle meurt
-                if (totalMasseAntilopeMangee < doubleMassePredateur){
+                //si le lion n'a pas mangé la masse suffisante elle meurt
+                if (doubleMassePredateur>0){
                     a.mourir();
                 }
               
-            } else{
-                // sinon on fait manger l'antilope
-                //une antilope doit manger 2 fois sa masse d’herbe 
-                
-                //ou manger
-                //A completer
-                // s'il ya pas assez d'herbe l'antilope meurt
-                if(this.herbe.getMasseAnnuelle() >= a.getMasse() *2){
-                    //this.masseAnnuelle = 0.6 * this.masse;
-                    double nouvelleMasseHerbe = this.herbe.getMasseAnnuelle() - (2 * a.getMasse()) ;
-                    this.herbe.setMasseAnnuelle(nouvelleMasseHerbe);
-                } else{
+            }
+            // sinon on fait manger l'antilope
+            else if (a.estProie() & a.estVivant()){
+                //une antilope doit manger 2 fois sa masse d’herbe, s'il ya pas assez d'herbe l'antilope meurt
+                if(this.herbe.getMasseAnnuelle() >= a.getMasse()*2){
+                    a.manger(this.herbe);
+                }
+                else{
                    a.mourir();
                 }
-            }                          
-        }             
-        retirerAnimauxMorts();
-    }
-    
-    public ArrayList<Animal> getIndividus(){
-        return this.individus;
-    }
 
+            } 
+
+        }
+        retirerAimauxMorts(); 
+                    
+
+    } 
+
+    
     // reproducing
-
-    // public void reproduire(){
-    //     // la moitie du nombre d'animaux matures sont femmelles et peuvent reproduire
-    //     int nombreDeFemmellesAntilope = getNombreProiesMatures()/2;
-    //     int nombreDeFemmellesLionne = getNombrePredateursMatures()/2;
-    //     for (int i =0; i<nombreDeFemmellesAntilope; i++){
-    //     this.individus.add(new Animal());// new antilope
-        
-   
-    // }
-    // for (int i =0; i<nombreDeFemmellesLionne; i++){
-    
-    //      this.individus.add(new Animal());//new Lion // accoucher
-    // }
-        
-    // }
-
-    public void reproduire()
-    {
-	    ArrayList<Animal> proies = new ArrayList<>();
-	    ArrayList<Animal> predateurs = new ArrayList<>();
-
-	    boolean femelleLionne = false;
-	    boolean femelleaAntilope = false;
-	    for(Animal animal : this.individus) {
-            if(animal.estVivant() && animal.estMature()) {
-                if(animal.estPredateur()) {
-                    if(femelleLionne) {
-                        Lion lion = (Lion)animal.accoucher();
-                         predateurs.add(lion);
-                     }
-                     femelleLionne = !femelleLionne;
-                } else if(animal.estProie()) {
-                    if(femelleaAntilope) {
-                        Antilope antilope = (Antilope)animal.accoucher();
-                        proies.add(antilope);
-                    }
-                    femelleaAntilope = !femelleaAntilope;
-                }
+    public void reproduire(){
+        // la moitie du nombre d'animaux matures sont femmelles et peuvent reproduire
+        int bebeProies = this.getNombreProiesMatures()/2;
+        int bebePredateurs = this.getNombrePredateursMatures()/2;
+        for(int i = 0; i<this.individus.size(); i++){
+            if(this.individus.get(i).estProie() & this.individus.get(i).estVivant()& bebeProies>0 & this.individus.get(i).estMature()) {
+                this.individus.add(this.individus.get(i).accoucher());
+                bebeProies--;
+            }
+            else if( this.individus.get(i).estPredateur() & this.individus.get(i).estVivant()& bebePredateurs>0 & this.individus.get(i).estMature()){
+                this.individus.add(this.individus.get(i).accoucher());
+                bebePredateurs--;
             }
         }
-
-        for(Animal animal : proies) this.individus.add(animal);
-        for(Animal animal : predateurs) this.individus.add(animal);
-        retirerAnimauxMorts();
+        retirerAimauxMorts();        
     }
-
-
-
-
     // mix the list of animals
     public void melanger(){
         Collections.shuffle(this.individus, new Random(4));
-    }   
+    }  
+    
+    public ArrayList<Animal> getIndividus(){
+     
+        return this.individus;
+    }
 
-    public void retirerAnimauxMorts(){
-        for(int i = this.individus.size() - 1; i >= 0; --i) {
+    // remove dead animals
+    public void retirerAimauxMorts(){        
+        for(int i = this.individus.size() - 1; i >= 0; i--) {
             if((!this.individus.get(i).estVivant())){
                 this.individus.remove(i);
             }
         }
     }
+   
 }
-
 
